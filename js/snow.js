49d289a07e64a03e26cdd493d6fbc77737b9e329
leeliner <59076056+leeliner@users.noreply.github.com>
2020-04-25 18:22:02 +0800

    Site updated: 2020-04-25 18:21:56

--- js/snow.js
+++ js/snow.js
@@ -34,7 +34,7 @@ function snowCanvas() {
     /* 添加Dom结点 */
     var snowcanvas = document.createElement("canvas");
     snowcanvas.id = "snowfall";
-    snowcanvas.width = window.innerWidth;
+    snowcanvas.width = 900;
     snowcanvas.height = document.body.clientHeight;
     snowcanvas.setAttribute("style", "position:absolute; top: 0; left: 0; z-index: 1; pointer-events: none;");
     document.getElementsByTagName("body")[0].appendChild(snowcanvas);
